f8bcd2347eda55524f706047c3d8f94b61444444
William Earnhardt <wearnhardt@gmail.com>
2018-08-07 14:09:09 -0400

    WIP

--- lib/meta-box-partial-page.php
+++ lib/meta-box-partial-page.php
@@ -87,6 +87,7 @@ add_filter( 'redirect_post_location', 'gutenberg_meta_box_save_redirect', 10, 2 
  * @return array Meta box data without core meta boxes.
  */
 function gutenberg_filter_meta_boxes( $meta_boxes ) {
+
 	$core_side_meta_boxes = array(
 		'submitdiv',
 		'formatdiv',
@@ -113,7 +114,7 @@ function gutenberg_filter_meta_boxes( $meta_boxes ) {
 		'revisionsdiv',
 		'postexcerpt',
 		'trackbacksdiv',
-		'postcustom',
+//		'postcustom',
 		'commentstatusdiv',
 		'commentsdiv',
 		'slugdiv',
@@ -149,6 +150,7 @@ function gutenberg_filter_meta_boxes( $meta_boxes ) {
 
 	return $meta_boxes;
 }
+
 
 add_filter( 'filter_gutenberg_meta_boxes', 'gutenberg_filter_meta_boxes' );
 
@@ -371,4 +373,39 @@ function gutenberg_meta_box_post_form_hidden_fields( $post ) {
 	wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false );
 	// Permalink title nonce.
 	wp_nonce_field( 'samplepermalink', 'samplepermalinknonce', false );
+}
+
+
+/**
+ * Display custom fields form fields.
+ *
+ * @since 2.6.0
+ *
+ * @param object $post
+ */
+function post_custom_meta_box( $post ) {
+	?>
+	<div id="postcustomstuff">
+		<div id="ajax-response"></div>
+		<?php
+		$metadata = has_meta( $post->ID );
+		foreach ( $metadata as $key => $value ) {
+			if ( is_protected_meta( $metadata[ $key ]['meta_key'], 'post' ) || ! current_user_can( 'edit_post_meta', $post->ID, $metadata[ $key ]['meta_key'] ) ) {
+				unset( $metadata[ $key ] );
+			}
+		}
+		list_meta( $metadata );
+		meta_form( $post );
+		?>
+	</div>
+	<p>
+		<?php
+		printf(
+		/* translators: %s: Codex URL */
+			__( 'Custom fields can be used to add extra metadata to a post that you can <a href="%s">use in your theme</a>.' ),
+			__( 'https://codex.wordpress.org/Using_Custom_Fields' )
+		);
+		?>
+	</p>
+	<?php
 }
